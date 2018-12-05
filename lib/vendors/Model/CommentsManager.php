6275639b3c1fa397e91546ec147be95dff330ad8
BIRNA GERALD <birnagerald.pro@gmail.com>
2018-12-05 01:20:19 +0100

    Ajout des fonctions de gérance news et commentaires dans les class abs

--- lib/vendors/Model/CommentsManager.php
+++ lib/vendors/Model/CommentsManager.php
@@ -1,7 +1,7 @@
 <?php
 namespace Model;
  
-use \Fram\Manager;
+use \OCFram\Manager;
 use \Entity\Comment;
  
 abstract class CommentsManager extends Manager
@@ -12,6 +12,20 @@ abstract class CommentsManager extends Manager
    * @return void
    */
   abstract protected function add(Comment $comment);
+ 
+  /**
+   * Méthode permettant de supprimer un commentaire.
+   * @param $id L'identifiant du commentaire à supprimer
+   * @return void
+   */
+  abstract public function delete($id);
+ 
+  /**
+   * Méthode permettant de supprimer tous les commentaires liés à une news
+   * @param $news L'identifiant de la news dont les commentaires doivent être supprimés
+   * @return void
+   */
+  abstract public function deleteFromNews($news);
  
   /**
    * Méthode permettant d'enregistrer un commentaire.
@@ -36,4 +50,18 @@ abstract class CommentsManager extends Manager
    * @return array
    */
   abstract public function getListOf($news);
+ 
+  /**
+   * Méthode permettant de modifier un commentaire.
+   * @param $comment Le commentaire à modifier
+   * @return void
+   */
+  abstract protected function modify(Comment $comment);
+ 
+  /**
+   * Méthode permettant d'obtenir un commentaire spécifique.
+   * @param $id L'identifiant du commentaire
+   * @return Comment
+   */
+  abstract public function get($id);
 }
