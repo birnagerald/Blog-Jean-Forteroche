33a227675fbfe4b38bf4f1e27718a5d3a8ae4030
BIRNA GERALD <birnagerald.pro@gmail.com>
2018-12-05 19:46:30 +0100

    Update du controller, de la view pour tester nos class (formulaire)

--- App/Frontend/Modules/News/NewsController.php
+++ App/Frontend/Modules/News/NewsController.php
@@ -1,73 +1,52 @@
 <?php
 namespace App\Frontend\Modules\News;
 
-use \Entity\Comment;
 use \Fram\BackController;
 use \Fram\HTTPRequest;
+use \Entity\Comment;
+use \Fram\Form;
+use \Fram\StringField;
+use \Fram\TextField;
 
 class NewsController extends BackController
 {
-    public function executeIndex(HTTPRequest $request)
+  public function executeInsertComment(HTTPRequest $request)
+  {
+    // Si le formulaire a été envoyé, on crée le commentaire avec les valeurs du formulaire.
+    if ($request->method() == 'POST')
     {
-        $nombreNews = $this->app->config()->get('nombre_news');
-        $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
-
-        // On ajoute une définition pour le titre.
-        $this->page->addVar('title', 'Liste des ' . $nombreNews . ' dernières news');
-
-        // On récupère le manager des news.
-        $manager = $this->managers->getManagerOf('News');
-
-        $listeNews = $manager->getList(0, $nombreNews);
-
-        foreach ($listeNews as $news) {
-            if (strlen($news->contenu()) > $nombreCaracteres) {
-                $debut = substr($news->contenu(), 0, $nombreCaracteres);
-                $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
-
-                $news->setContenu($debut);
-            }
-        }
-
-        // On ajoute la variable $listeNews à la vue.
-        $this->page->addVar('listeNews', $listeNews);
+      $comment = new Comment([
+        'news' => $request->getData('news'),
+        'auteur' => $request->postData('auteur'),
+        'contenu' => $request->postData('contenu')
+      ]);
     }
-
-    public function executeShow(HTTPRequest $request)
+    else
     {
-        $news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
-
-        if (empty($news)) {
-            $this->app->httpResponse()->redirect404();
-        }
-
-        $this->page->addVar('title', $news->titre());
-        $this->page->addVar('news', $news);
-        $this->page->addVar('comments', $this->managers->getManagerOf('Comments')->getListOf($news->id()));
+      $comment = new Comment;
     }
-
-    public function executeInsertComment(HTTPRequest $request)
+    
+    $form = new Form($comment);
+    
+    $form->add(new StringField([
+        'label' => 'Auteur',
+        'name' => 'auteur',
+        'maxLength' => 50,
+       ]))
+       ->add(new TextField([
+        'label' => 'Contenu',
+        'name' => 'contenu',
+        'rows' => 7,
+        'cols' => 50,
+       ]));
+    
+    if ($form->isValid())
     {
-        $this->page->addVar('title', 'Ajout d\'un commentaire');
-
-        if ($request->postExists('pseudo')) {
-            $comment = new Comment([
-                'news' => $request->getData('news'),
-                'auteur' => $request->postData('pseudo'),
-                'contenu' => $request->postData('contenu'),
-            ]);
-
-            if ($comment->isValid()) {
-                $this->managers->getManagerOf('Comments')->save($comment);
-
-                $this->app->user()->setFlash('Le commentaire a bien été ajouté, merci !');
-
-                $this->app->httpResponse()->redirect('news-' . $request->getData('news') . '.html');
-            } else {
-                $this->page->addVar('erreurs', $comment->erreurs());
-            }
-
-            $this->page->addVar('comment', $comment);
-        }
+      // On enregistre le commentaire
     }
+    
+    $this->page->addVar('comment', $comment);
+    $this->page->addVar('form', $form->createView()); // On passe le formulaire généré à la vue.
+    $this->page->addVar('title', 'Ajout d\'un commentaire');
+  }
 }
