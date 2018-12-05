0ad5fed1a3fdc49f3e8b79924b20f3cf355945fd
BIRNA GERALD <birnagerald.pro@gmail.com>
2018-12-05 20:53:48 +0100

    update du controller frontend pour l'ajout de commentaire

--- App/Frontend/Modules/News/NewsController.php
+++ App/Frontend/Modules/News/NewsController.php
@@ -1,18 +1,59 @@
 <?php
 namespace App\Frontend\Modules\News;
-
+ 
 use \Fram\BackController;
 use \Fram\HTTPRequest;
 use \Entity\Comment;
-use \Fram\Form;
-use \Fram\StringField;
-use \Fram\TextField;
-
+use \FormBuilder\CommentFormBuilder;
+use \Fram\FormHandler;
+ 
 class NewsController extends BackController
 {
+  public function executeIndex(HTTPRequest $request)
+  {
+    $nombreNews = $this->app->config()->get('nombre_news');
+    $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
+ 
+    // On ajoute une définition pour le titre.
+    $this->page->addVar('title', 'Liste des '.$nombreNews.' dernières news');
+ 
+    // On récupère le manager des news.
+    $manager = $this->managers->getManagerOf('News');
+ 
+    $listeNews = $manager->getList(0, $nombreNews);
+ 
+    foreach ($listeNews as $news)
+    {
+      if (strlen($news->contenu()) > $nombreCaracteres)
+      {
+        $debut = substr($news->contenu(), 0, $nombreCaracteres);
+        $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
+ 
+        $news->setContenu($debut);
+      }
+    }
+ 
+    // On ajoute la variable $listeNews à la vue.
+    $this->page->addVar('listeNews', $listeNews);
+  }
+ 
+  public function executeShow(HTTPRequest $request)
+  {
+    $news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
+ 
+    if (empty($news))
+    {
+      $this->app->httpResponse()->redirect404();
+    }
+ 
+    $this->page->addVar('title', $news->titre());
+    $this->page->addVar('news', $news);
+    $this->page->addVar('comments', $this->managers->getManagerOf('Comments')->getListOf($news->id()));
+  }
+ 
   public function executeInsertComment(HTTPRequest $request)
   {
-    // Si le formulaire a été envoyé, on crée le commentaire avec les valeurs du formulaire.
+    // Si le formulaire a été envoyé.
     if ($request->method() == 'POST')
     {
       $comment = new Comment([
@@ -25,28 +66,23 @@ class NewsController extends BackController
     {
       $comment = new Comment;
     }
-    
-    $form = new Form($comment);
-    
-    $form->add(new StringField([
-        'label' => 'Auteur',
-        'name' => 'auteur',
-        'maxLength' => 50,
-       ]))
-       ->add(new TextField([
-        'label' => 'Contenu',
-        'name' => 'contenu',
-        'rows' => 7,
-        'cols' => 50,
-       ]));
-    
-    if ($form->isValid())
+ 
+    $formBuilder = new CommentFormBuilder($comment);
+    $formBuilder->build();
+ 
+    $form = $formBuilder->form();
+ 
+    $formHandler = new FormHandler($form, $this->managers->getManagerOf('Comments'), $request);
+ 
+    if ($formHandler->process())
     {
-      // On enregistre le commentaire
+      $this->app->user()->setFlash('Le commentaire a bien été ajouté, merci !');
+ 
+      $this->app->httpResponse()->redirect('news-'.$request->getData('news').'.html');
     }
-    
+ 
     $this->page->addVar('comment', $comment);
-    $this->page->addVar('form', $form->createView()); // On passe le formulaire généré à la vue.
+    $this->page->addVar('form', $form->createView());
     $this->page->addVar('title', 'Ajout d\'un commentaire');
   }
 }
