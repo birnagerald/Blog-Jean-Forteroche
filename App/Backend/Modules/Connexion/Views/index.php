973bcb5bb565e0d0939b48e9e9746fac7e290d7d
BIRNA GERALD <birnagerald.pro@gmail.com>
2018-12-06 02:40:07 +0100

    update de la view de connexion

--- App/Backend/Modules/Connexion/Views/index.php
+++ App/Backend/Modules/Connexion/Views/index.php
@@ -1,11 +1,13 @@
 <h2>Connexion</h2>
- 
+
 <form action="" method="post">
-  <label>Pseudo</label>
-  <input type="text" name="login" /><br />
- 
-  <label>Mot de passe</label>
-  <input type="password" name="password" /><br /><br />
- 
-  <input type="submit" value="Connexion" />
+  <div class="form-group">
+    <label>Pseudo</label>
+    <input type="text" name="login" minlength="3" maxlength="10" class="form-control" /><br />
+
+    <label>Mot de passe</label>
+    <input type="password" name="password" minlength="4" maxlength="15" class="form-control" /><br /><br />
+
+    <input type="submit" value="Connexion" class="btn btn-primary" />
+  </div>
 </form>
