7a825b8dd9fe84c50f5794b4eb49fa8f245ccb83
BIRNA GERALD <birnagerald.pro@gmail.com>
2018-12-06 03:07:10 +0100

    update de class user (logout() + deconnexion auto après 35 min)

--- lib/Fram/User.php
+++ lib/Fram/User.php
@@ -3,48 +3,76 @@ namespace Fram;
  
 session_start();
  
+$now = time();
+
+if (isset($_SESSION['expire']) && $now > $_SESSION['expire']) {
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    session_destroy();
+
+}
+
 class User
 {
-  public function getAttribute($attr)
-  {
-    return isset($_SESSION[$attr]) ? $_SESSION[$attr] : null;
-  }
- 
-  public function getFlash()
-  {
-    $flash = $_SESSION['flash'];
-    unset($_SESSION['flash']);
- 
-    return $flash;
-  }
- 
-  public function hasFlash()
-  {
-    return isset($_SESSION['flash']);
-  }
- 
-  public function isAuthenticated()
-  {
-    return isset($_SESSION['auth']) && $_SESSION['auth'] === true;
-  }
- 
-  public function setAttribute($attr, $value)
-  {
-    $_SESSION[$attr] = $value;
-  }
- 
-  public function setAuthenticated($authenticated = true)
-  {
-    if (!is_bool($authenticated))
+    public function getAttribute($attr)
     {
-      throw new \InvalidArgumentException('La valeur spécifiée à la méthode User::setAuthenticated() doit être un boolean');
+        return isset($_SESSION[$attr]) ? $_SESSION[$attr] : null;
     }
- 
-    $_SESSION['auth'] = $authenticated;
-  }
- 
-  public function setFlash($value)
-  {
-    $_SESSION['flash'] = $value;
-  }
+
+    public function getFlash()
+    {
+        $flash = $_SESSION['flash'];
+        unset($_SESSION['flash']);
+
+        return $flash;
+    }
+
+    public function hasFlash()
+    {
+        return isset($_SESSION['flash']);
+    }
+
+    public function isAuthenticated()
+    {
+        return isset($_SESSION['auth']) && $_SESSION['auth'] === true;
+    }
+
+    public function setAttribute($attr, $value)
+    {
+        $_SESSION[$attr] = $value;
+    }
+
+    public function setAuthenticated($authenticated = true)
+    {
+        if (!is_bool($authenticated)) {
+            throw new \InvalidArgumentException('La valeur spécifiée à la méthode User::setAuthenticated() doit être un boolean');
+        }
+
+        $_SESSION['auth'] = $authenticated;
+
+    }
+
+    public function setFlash($value)
+    {
+        $_SESSION['flash'] = $value;
+    }
+
+    public function logout()
+    {
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+        session_destroy();
+
+    }
+
 }
