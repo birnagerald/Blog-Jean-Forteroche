615840777f8e0d82192e9efc074af3114facf85f
BIRNA GERALD <birnagerald.pro@gmail.com>
2018-12-07 22:08:41 +0100

    fix indentation newsController + update app (nombre_caracteres)

--- App/Frontend/Modules/News/NewsController.php
+++ App/Frontend/Modules/News/NewsController.php
@@ -1,17 +1,17 @@
 <?php
 namespace App\Frontend\Modules\News;
- 
-use \Fram\BackController;
-use \Fram\HTTPRequest;
+
 use \Entity\Comment;
 use \FormBuilder\CommentFormBuilder;
+use \Fram\BackController;
 use \Fram\FormHandler;
- 
+use \Fram\HTTPRequest;
+
 class NewsController extends BackController
 {
-  public function executeIndex(HTTPRequest $request)
-  {
-    $nombreNews = $this->app->config()->get('nombre_news');
+    public function executeIndex(HTTPRequest $request)
+    {
+        $nombreNews = $this->app->config()->get('nombre_news');
         $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
 
         // On ajoute une définition pour le titre.
@@ -28,7 +28,6 @@ class NewsController extends BackController
         $lastPage = end($paginationLien);
 
         $pagination = $request->getData('pagination');
-        
 
         if ($pagination == null) {
             $pagination = 1;
@@ -60,54 +59,49 @@ class NewsController extends BackController
         $this->page->addVar('paginationLien', $paginationLien);
         $this->page->addVar('currentPage', $currentPage);
         $this->page->addVar('lastPage', $lastPage);
-  }
- 
-  public function executeShow(HTTPRequest $request)
-  {
-    $news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
- 
-    if (empty($news))
-    {
-      $this->app->httpResponse()->redirect404();
     }
- 
-    $this->page->addVar('title', $news->titre());
-    $this->page->addVar('news', $news);
-    $this->page->addVar('comments', $this->managers->getManagerOf('Comments')->getListOf($news->id()));
-  }
- 
-  public function executeInsertComment(HTTPRequest $request)
-  {
-    // Si le formulaire a été envoyé.
-    if ($request->method() == 'POST')
+
+    public function executeShow(HTTPRequest $request)
     {
-      $comment = new Comment([
-        'news' => $request->getData('news'),
-        'auteur' => $request->postData('auteur'),
-        'contenu' => $request->postData('contenu')
-      ]);
+        $news = $this->managers->getManagerOf('News')->getUnique($request->getData('id'));
+
+        if (empty($news)) {
+            $this->app->httpResponse()->redirect404();
+        }
+
+        $this->page->addVar('title', $news->titre());
+        $this->page->addVar('news', $news);
+        $this->page->addVar('comments', $this->managers->getManagerOf('Comments')->getListOf($news->id()));
     }
-    else
+
+    public function executeInsertComment(HTTPRequest $request)
     {
-      $comment = new Comment;
+        // Si le formulaire a été envoyé.
+        if ($request->method() == 'POST') {
+            $comment = new Comment([
+                'news' => $request->getData('news'),
+                'auteur' => $request->postData('auteur'),
+                'contenu' => $request->postData('contenu'),
+            ]);
+        } else {
+            $comment = new Comment;
+        }
+
+        $formBuilder = new CommentFormBuilder($comment);
+        $formBuilder->build();
+
+        $form = $formBuilder->form();
+
+        $formHandler = new FormHandler($form, $this->managers->getManagerOf('Comments'), $request);
+
+        if ($formHandler->process()) {
+            $this->app->user()->setFlash('Le commentaire a bien été ajouté, merci !');
+
+            $this->app->httpResponse()->redirect('news-' . $request->getData('news') . '.html');
+        }
+
+        $this->page->addVar('comment', $comment);
+        $this->page->addVar('form', $form->createView());
+        $this->page->addVar('title', 'Ajout d\'un commentaire');
     }
- 
-    $formBuilder = new CommentFormBuilder($comment);
-    $formBuilder->build();
- 
-    $form = $formBuilder->form();
- 
-    $formHandler = new FormHandler($form, $this->managers->getManagerOf('Comments'), $request);
- 
-    if ($formHandler->process())
-    {
-      $this->app->user()->setFlash('Le commentaire a bien été ajouté, merci !');
- 
-      $this->app->httpResponse()->redirect('news-'.$request->getData('news').'.html');
-    }
- 
-    $this->page->addVar('comment', $comment);
-    $this->page->addVar('form', $form->createView());
-    $this->page->addVar('title', 'Ajout d\'un commentaire');
-  }
 }
