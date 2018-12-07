9fcb83de96a56c8cea8f096ae04ce13eeab8d317
BIRNA GERALD <birnagerald.pro@gmail.com>
2018-12-07 20:43:53 +0100

    Ajout Pagination Acceuil

--- App/Frontend/Modules/News/NewsController.php
+++ App/Frontend/Modules/News/NewsController.php
@@ -12,29 +12,54 @@ class NewsController extends BackController
   public function executeIndex(HTTPRequest $request)
   {
     $nombreNews = $this->app->config()->get('nombre_news');
-    $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
- 
-    // On ajoute une définition pour le titre.
-    $this->page->addVar('title', 'Liste des '.$nombreNews.' dernières news');
- 
-    // On récupère le manager des news.
-    $manager = $this->managers->getManagerOf('News');
- 
-    $listeNews = $manager->getList(0, $nombreNews);
- 
-    foreach ($listeNews as $news)
-    {
-      if (strlen($news->contenu()) > $nombreCaracteres)
-      {
-        $debut = substr($news->contenu(), 0, $nombreCaracteres);
-        $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
- 
-        $news->setContenu($debut);
-      }
-    }
- 
-    // On ajoute la variable $listeNews à la vue.
-    $this->page->addVar('listeNews', $listeNews);
+        $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
+
+        // On ajoute une définition pour le titre.
+        $this->page->addVar('title', 'Acceuil');
+
+        // On récupère le manager des news.
+        $manager = $this->managers->getManagerOf('News');
+        $paginationLien = [];
+        $nombreDePages = ceil($this->managers->getManagerOf('News')->count() / $nombreNews);
+
+        for ($i = 0; $i < $nombreDePages; $i++) {
+            $paginationLien[$i] = $i + 1;
+        }
+        $lastPage = end($paginationLien);
+
+        $pagination = $request->getData('pagination');
+        
+
+        if ($pagination == null) {
+            $pagination = 1;
+        }
+        $currentPage = $pagination;
+
+        if ($pagination > $nombreDePages) {
+            $this->app->httpResponse()->redirect404();
+        } else {
+            $pagination = (($pagination - 1) * 5);
+
+        }
+
+        $listeNews = $manager->getList($pagination, $nombreNews);
+
+        foreach ($listeNews as $news) {
+            if (strlen($news->contenu()) > $nombreCaracteres) {
+                $debut = substr($news->contenu(), 0, $nombreCaracteres);
+                $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
+
+                $news->setContenu($debut);
+            }
+        }
+
+        // On ajoute la variable $listeNews à la vue.
+        $this->page->addVar('listeNews', $listeNews);
+
+        // On ajoute la variable $paginationLien et $currentPage et $lastPage à la vue.
+        $this->page->addVar('paginationLien', $paginationLien);
+        $this->page->addVar('currentPage', $currentPage);
+        $this->page->addVar('lastPage', $lastPage);
   }
  
   public function executeShow(HTTPRequest $request)
--- App/Frontend/Modules/News/Views/index.php
+++ App/Frontend/Modules/News/Views/index.php
@@ -1,8 +1,53 @@
 <?php
-foreach ($listeNews as $news)
-{
-?>
-  <h2><a href="news-<?= $news['id'] ?>.html"><?= $news['titre'] ?></a></h2>
-  <p><?= nl2br($news['contenu']) ?></p>
+foreach ($listeNews as $news) {
+    ?>
+  <h2><a href="news-<?=$news['id']?>.html"><?=$news['titre']?></a></h2>
+  <p><?=nl2br($news['contenu'])?></p>
+
 <?php
 }
+?>
+
+<nav aria-label="Page navigation example">
+  <ul class="pagination">
+  <?php
+
+if ($currentPage == 1) {
+    ?>
+    <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+    <?php
+} else {
+    ?>
+    <li class="page-item"><a class="page-link" href="http://monsupersite/p-<?=($currentPage - 1)?>">Previous</a></li>
+    <?php
+}
+
+foreach ($paginationLien as $page) {
+
+    if ($currentPage == $page) {
+        ?>
+    <li class="page-item active"><a class="page-link" href="http://monsupersite/p-<?=$page?>"><?=$page?></a></li>
+    <?php
+}   else {
+        ?>
+    <li class="page-item"><a class="page-link" href="http://monsupersite/p-<?=$page?>"><?=$page?></a></li>
+
+  <?php
+
+    }
+
+}
+
+if ($currentPage == $lastPage) {
+    ?>
+
+  <li class="page-item disabled"><a class="page-link" href="#">Next</a></li>
+  <?php
+} else {
+    ?>
+  <li class="page-item"><a class="page-link" href="http://monsupersite/p-<?=$currentPage + 1?>">Next</a></li>
+  <?php
+}
+?>
+</ul>
+</nav>
