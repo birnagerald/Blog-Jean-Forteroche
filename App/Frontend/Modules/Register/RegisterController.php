eb5730c9d38cf2d5656b0c608cc34d0fc3e402ed
BIRNA GERALD <birnagerald.pro@gmail.com>
2018-12-18 05:43:49 +0100

    fix register form

--- App/Frontend/Modules/Register/RegisterController.php
+++ App/Frontend/Modules/Register/RegisterController.php
@@ -10,27 +10,27 @@ class RegisterController extends BackController
 
     public function executeIndex(HTTPRequest $request)
     {
-        // if ($request->method() == 'POST') {
+        if ($request->method() == 'POST') {
 
-        //     if (!empty($request->postData('name')) && !empty($request->postData('password'))) {
-        //         $hash = password_hash($request->postData('password'), PASSWORD_BCRYPT);
-        //         $users = new Users([
-        //             'name' => $request->postData('name'),
-        //             'password' => $hash,
-        //         ]);
+            if (!empty($request->postData('name')) && !empty($request->postData('password'))) {
+                $hash = password_hash($request->postData('password'), PASSWORD_BCRYPT);
+                $users = new Users([
+                    'name' => $request->postData('name'),
+                    'password' => $hash,
+                ]);
 
-        //         $this->managers->getManagerOf('Users')->add($users);
+                $this->managers->getManagerOf('Users')->add($users);
 
-        //         $this->app->user()->setFlash('Vous êtes maintenant inscrit !');
-        //         $this->app->httpResponse()->redirect('.');
-        //     }
-        //     else{
-        //         $this->app->user()->setFlash('Il faut remplir les champs !');
-        //     }
+                $this->app->user()->setFlash('Vous êtes maintenant inscrit !');
+                $this->app->httpResponse()->redirect('.');
+            }
+            else{
+                $this->app->user()->setFlash('Il faut remplir les champs !');
+            }
 
-        // }
+        }
         //  Décommenter cette ligne et commenter executeIndex() pour désactiver la fonctionnalité d'inscription
-         $this->app->httpResponse()->redirectDisable();
+        //  $this->app->httpResponse()->redirectDisable();
     }
 
 }
--- App/Frontend/Modules/Register/Views/index.php
+++ App/Frontend/Modules/Register/Views/index.php
@@ -1,9 +1,14 @@
 <h2>Inscription</h2>
 <hr>
+<?php if ($user->hasFlash()) {
+    echo '<p style="text-align: center;">', $user->getFlash(), '</p>';
+}
+?>
+
 <form method="post" action="">
     <div class="form-group">
-		<input class="form-control" minlength="3" maxlength="10" name="name" placeholder="Name..." class="form-control" ><br>
-		<input class="form-control" minlength="4" maxlength="15" type="password" placeholder="Password..." class="form-control" ><br>
+		<input class="form-control" minlength="3" maxlength="10" name="name" placeholder="Name..."><br>
+		<input class="form-control" minlength="4" maxlength="15" type="password" name="password" placeholder="Password..."><br>
 		<input class="btn btn-primary" name="submit" type="submit" value="Envoyer"><br>
     </div>
 </form>
